fa660c379b079e8f3ee5f0a8596ed3851bee98d5
李文强 <codelove1314@live.cn>
2018-06-14 14:55:38 +0800

    初始化

---
+++ Magicodes.SwaggerUI/Magicodes.SwaggerUI/Extentions.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magicodes.SwaggerUI
+{
+    public static class Extentions
+    {
+        /// <summary>
+        /// 添加自定义API文档生成(支持文档配置)
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <param name="hostingEnvironment"></param>
+        public static void AddCustomSwaggerGen(this IServiceCollection services, IConfigurationRoot configuration, IHostingEnvironment hostingEnvironment)
+        {
+            if (configuration == null || configuration["SwaggerDoc:IsEnabled"] == null)
+            {
+                return;
+            }
+            if (bool.Parse(configuration["SwaggerDoc:IsEnabled"]))
+            {
+                //设置API文档生成
+                services.AddSwaggerGen(options =>
+                {
+                    options.DescribeAllEnumsAsStrings();
+                    options.SwaggerDoc("v1", new Info
+                    {
+                        Title = configuration["SwaggerDoc:Title"],
+                        Version = configuration["SwaggerDoc:Version"],
+                        Description = configuration["SwaggerDoc:Description"],
+                        Contact = new Contact
+                        {
+                            Name = configuration["SwaggerDoc:Contact:Name"],
+                            Email = configuration["SwaggerDoc:Contact:Email"]
+                        }
+                    });
+
+                    if (bool.Parse(configuration["SwaggerDoc:Authorize"] ?? "false"))
+                        //以便于在界面上显示验证（Authorize）按钮，验证按钮处理逻辑基于 wwwroot/swagger/ui/index.html
+                        options.AddSecurityDefinition("Bearer", new BasicAuthScheme());
+
+                    //遍历所有xml并加载
+                    var paths = new List<string>();
+                    var plusPath = Path.Combine(hostingEnvironment.WebRootPath, "PlugIns");
+                    if (Directory.Exists(plusPath))
+                    {
+                        var xmlFiles = new DirectoryInfo(plusPath).GetFiles("*.Application.xml");
+                        foreach (var item in xmlFiles)
+                        {
+                            paths.Add(item.FullName);
+                        }
+                    }
+                    var binXmlFiles = new DirectoryInfo(hostingEnvironment.ContentRootPath).GetFiles("*.Application.xml", hostingEnvironment.EnvironmentName == "Development" ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
+                    foreach (var item in binXmlFiles)
+                    {
+                        paths.Add(item.FullName);
+                    }
+                    binXmlFiles = new DirectoryInfo(hostingEnvironment.ContentRootPath).GetFiles("*.Host.xml", hostingEnvironment.EnvironmentName == "Development" ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
+                    foreach (var item in binXmlFiles)
+                    {
+                        paths.Add(item.FullName);
+                    }
+
+                    foreach (var filePath in paths)
+                    {
+                        options.IncludeXmlComments(filePath);
+                    }
+                    options.DocInclusionPredicate((docName, description) => true);
+                    options.DocumentFilter<HiddenApiFilter>(configuration);
+
+                    if (configuration["SwaggerDoc:UseFullNameForSchemaId"] != null && bool.Parse(configuration["SwaggerDoc:UseFullNameForSchemaId"]))
+                    {
+                        //使用全名作为架构id
+                        options.CustomSchemaIds(p => p.FullName);
+                    }
+                });
+            }
+        }
+
+        /// <summary>
+        /// 启用自定义API文档(支持文档配置)
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="configuration"></param>
+        public static void UseCustomSwaggerUI(this IApplicationBuilder app, IConfigurationRoot configuration)
+        {
+            if (configuration == null || configuration["SwaggerDoc:IsEnabled"] == null)
+            {
+                return;
+            }
+            if (bool.Parse(configuration["SwaggerDoc:IsEnabled"]))
+            {
+                app.UseSwagger();
+                // 加载swagger-ui 资源 (HTML, JS, CSS etc.)
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", configuration["SwaggerDoc:Title"] ?? "App API V1");
+                    //允许通过嵌入式资源配置首页
+                    if (!string.IsNullOrWhiteSpace(configuration["SwaggerDoc:ManifestResourceUrl"]))
+                    {
+                        options.IndexStream = () => 
+                        Assembly.Load(configuration["SwaggerDoc:ManifestResourceAssembly"])
+                   .GetManifestResourceStream(configuration["SwaggerDoc:ManifestResourceUrl"]);
+                    }
+                });
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Magicodes.SwaggerUI
 快速配置和集成SwaggerUI
+Magicodes SwaggerUI扩展库
+
+配置Demo：
+```
+  "SwaggerDoc": {
+    "IsEnabled": "true",
+    "Title": "Api Documents",
+    "Version": "v1",
+    "Description": "",
+    "Contact": {
+      "Name": "心莱科技",
+      "Email": "xinlai@xin-lai.com"
+    },
+    //以便于在界面上显示验证（Authorize）按钮，验证按钮处理逻辑基于 wwwroot/swagger/ui/index.html
+    "Authorize": "true",
+    "HiddenApi": {
+      "IsEnabled": "false",
+      "HiddenUrls": ""
+    },
+    //设置api首页从嵌入资源中加载
+    "ManifestResourceAssembly": "Magicodes.Admin.Web.Host",
+    "ManifestResourceUrl": "Magicodes.Admin.Web.wwwroot.swagger.ui.index.html"
+  },
+```
